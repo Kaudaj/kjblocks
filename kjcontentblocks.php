220624c41a888e7b00b5647727b479afa264e2ea
Tom Combet <tom.combet@sfr.fr>
2022-06-20 13:11:15 +0200

    Start from kjmodulebedrock

---
+++ kjcontentblocks.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * Copyright since 2019 Kaudaj
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@kaudaj.com so we can send you a copy immediately.
+ *
+ * @author    Kaudaj <info@kaudaj.com>
+ * @copyright Since 2019 Kaudaj
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+declare(strict_types=1);
+
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+use Kaudaj\Module\ContentBlocks\Form\Settings\GeneralConfiguration;
+use PrestaShop\PrestaShop\Adapter\Configuration;
+use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class KJContentBlocks extends Module
+{
+    /**
+     * @var array<string, string> Configuration values
+     */
+    public const CONFIGURATION_VALUES = [
+        GeneralConfiguration::EXAMPLE_SETTING_KEY => 'default_value',
+    ];
+
+    /**
+     * @var string[] Hooks to register
+     */
+    public const HOOKS = [
+        'exampleHook',
+    ];
+
+    /**
+     * @var Configuration<string, mixed> Configuration
+     */
+    private $configuration;
+
+    public function __construct()
+    {
+        $this->name = 'kjcontentblocks';
+        $this->tab = 'others';
+        $this->version = '1.0.0';
+        $this->author = 'Kaudaj';
+        $this->ps_versions_compliancy = ['min' => '1.7.8.0', 'max' => _PS_VERSION_];
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->trans('Content Blocks', [], 'Modules.Kjcontentblocks.Admin');
+        $this->description = $this->trans(<<<EOF
+        Add your content blocks where you want them.
+EOF
+            ,
+            [],
+            'Modules.Kjcontentblocks.Admin'
+        );
+
+        $this->tabs = [
+            [
+                'name' => 'Content Blocks Settings',
+                'class_name' => 'KJContentBlocksSettings',
+                'route_name' => 'kj_content_blocks_settings',
+                'parent_class_name' => 'CONFIGURE',
+                'visible' => false,
+                'wording' => 'Content Blocks Settings',
+                'wording_domain' => 'Modules.Kjcontentblocks.Admin',
+            ],
+        ];
+
+        $this->configuration = new Configuration();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isUsingNewTranslationSystem(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(): bool
+    {
+        return parent::install()
+            && $this->installConfiguration()
+            && $this->registerHook(self::HOOKS)
+        ;
+    }
+
+    /**
+     * Install configuration values
+     */
+    private function installConfiguration(): bool
+    {
+        try {
+            foreach (self::CONFIGURATION_VALUES as $key => $default_value) {
+                $this->configuration->set($key, $default_value);
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(): bool
+    {
+        return parent::uninstall()
+            && $this->uninstallConfiguration()
+        ;
+    }
+
+    /**
+     * Uninstall configuration values
+     */
+    private function uninstallConfiguration(): bool
+    {
+        try {
+            foreach (array_keys(self::CONFIGURATION_VALUES) as $key) {
+                $this->configuration->remove($key);
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get module configuration page content
+     */
+    public function getContent(): void
+    {
+        $container = SymfonyContainer::getInstance();
+
+        if ($container != null) {
+            /** @var UrlGeneratorInterface */
+            $router = $container->get('router');
+
+            Tools::redirectAdmin($router->generate('kj_content_blocks_settings'));
+        }
+    }
+
+    /**
+     * Example hook
+     *
+     * @param array<string, mixed> $params Hook parameters
+     */
+    public function hookExampleHook(array $params): void
+    {
+        /* Do anything */
+    }
+}
---
+++ src/Form/Settings/GeneralType.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright since 2019 Kaudaj
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@kaudaj.com so we can send you a copy immediately.
+ *
+ * @author    Kaudaj <info@kaudaj.com>
+ * @copyright Since 2019 Kaudaj
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Kaudaj\Module\ContentBlocks\Form\Settings;
+
+use PrestaShopBundle\Form\Admin\Type\MultistoreConfigurationType;
+use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class GeneralType extends TranslatorAwareType
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param FormBuilderInterface<string, mixed> $builder
+     * @param array<string, mixed> $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('example_setting', TextType::class, [
+                'label' => $this->trans('Example setting', 'Modules.Kjcontentblocks.Admin'),
+                'help' => $this->trans('Help user to fill this field.', 'Modules.Kjcontentblocks.Admin'),
+                'required' => false,
+                'multistore_configuration_key' => GeneralConfiguration::EXAMPLE_SETTING_KEY,
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see MultistoreConfigurationTypeExtension
+     */
+    public function getParent(): string
+    {
+        return MultistoreConfigurationType::class;
+    }
+}
