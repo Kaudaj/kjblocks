a409d54f9a7dffdbd9fd0539a2947fc6f2e97d01
Kaudaj <tom.combet@sfr.fr>
2023-07-12 14:35:15 +0000

    Lower min compliancy

--- kjblocks.php
+++ kjblocks.php
@@ -53,7 +53,7 @@ class KJBlocks extends Module implements WidgetInterface
         $this->tab = 'others';
         $this->version = '1.1.0';
         $this->author = 'Kaudaj';
-        $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => '8.0.99'];
+        $this->ps_versions_compliancy = ['min' => '1.7.8.0', 'max' => '8.0.99'];
 
         $this->bootstrap = true;
 
